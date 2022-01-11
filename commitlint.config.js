50d48f63f579f182ea2681d336b4bdca5735f0b0
Presentation <presentation-dk@Macbook-Pro-DamianK.local>
2022-01-11 20:11:19 +0100

    chore(PROJ-142): add files

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-max-length": [2, "always", 10],
+    "subject-min-length": [2, "always", 2],
+    "scope-case": [1, "always", "upper-case"],
+  },
+};
